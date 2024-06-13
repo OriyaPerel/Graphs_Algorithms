55741af44757faa9ad8203131f59001779b19e89
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-13 14:41:15 +0300

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -5,22 +5,23 @@
 // id:322522806
 // email:oriyaperel18@gmail.com
 
-namespace ariel {
-class Graph {
-private:
-    std::vector<std::vector<int>> g;
-    int row;
-    int col;
-    bool directed;
+namespace ariel
+{
+    class Graph
+    {
+    private:
+        std::vector<std::vector<int>> matrix; // represent the adjacency matrix
+        int numVertices;
+        bool directed;
 
-public:
-    Graph();
-    Graph(bool directed);
-    const std::vector<std::vector<int>> &getGraph()const ;
-    void loadGraph(std::vector<std::vector<int>> g);
-    void printGraph();
-    bool isdirected()const ;
-    int getNumEdge()const;
-    size_t getNumVertices()const;
-};
+    public:
+        Graph(); // the default is directed graph
+        Graph(bool directed);
+        const std::vector<std::vector<int>> &getGraph() const;
+        void loadGraph(std::vector<std::vector<int>> g);
+        void printGraph();
+        bool isdirected() const;
+        int getNumEdge() const;
+        size_t getNumVertices() const;
+    };
 }
